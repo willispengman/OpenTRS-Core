fc846d4930ef7b2148190497129064fd6d960f40
willispengman <willispengman@gmail.com>
2026-06-21 15:21:02 +0800

    Initialize OpenTRS-Core project structure

---
+++ opentrs/decoder/csq_decoder.py
@@ -0,0 +1,13 @@
+"""
+OpenTRS-Core
+CSQ Decoder
+
+Author: Willis Peng
+"""
+
+class CSQDecoder:
+    def __init__(self, filename: str):
+        self.filename = filename
+
+def open(self) -> None:
+    print(f"Opening CSQ file: {self.filename}")
